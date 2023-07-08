5f936063f8857462c7d471ef0be4744c89023ec1
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-07-08 16:18:28 +0530

    semi_layout

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React from "react";
 import "./App.css";
+import Main from "./Main";
 import Navbar from "./commons/Navbar";
 import Card_data from "./Card_data";
+import { Route, Router } from "react-router-dom";
+import Login from "./auth/Login";
 
 export default function App() {
   return (
-    <>
-      <div className="main_div">
-        <Navbar />
-        <div className="img">
-          <div className="image_div">picture</div>
-        </div>
-        {/* <div className="login_div">
-          <Login />
-        </div> */}
-        <div className="brand">
-          <div className="brand_div">
-            {" "}
-            <h1> Brands Best Sellers </h1>
-          </div>
-        </div>
-        <div>
-          <Card_data />
-        </div>
-        <div className="abt_div">
-          <h1> ABOUT US</h1>
-        </div>
-      </div>
-    </>
+    <Router>
+      <Route path="" element={Main} />
+      <Route path="/login" element={Login} />
+    </Router>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { Route, Router } from "react-router-dom";
-import Login from "./auth/Login";
+
+import Navbar from "./commons/Navbar";
+import Card_data from "./Card_data";
 
 export default function Main() {
   return (
-    <Router>
-      <Route path="/login" element={Login} />
-    </Router>
+    <>
+      <div className="main_div">
+        <Navbar />
+        <div className="img">
+          <div className="image_div">picture</div>
+        </div>
+        {/* <div className="login_div">
+            <Login />
+          </div> */}
+        <div className="brand">
+          <div className="brand_div">
+            {" "}
+            <h1> Brands Best Sellers </h1>
+          </div>
+        </div>
+        <div>
+          <Card_data />
+        </div>
+        <div className="abt_div">
+          <h1> ABOUT US</h1>
+        </div>
+      </div>
+    </>
   );
 }

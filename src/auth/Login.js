5499fc4b06b4cb2996d3327f7f4ded1f84f1f848
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-07-21 14:01:04 +0530

    login_layout_done_final

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,46 +1,18 @@
-import React, { useState } from "react";
-import { Button, Form, Input, Menu } from "semantic-ui-react";
-import "../commons/styles/Login.css";
+import React from "react";
+import { Form } from "semantic-ui-react";
+import "./styles/Login.css";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import Signup from "./Signup";
 
 export default function Login() {
-  const [data, setData] = useState({});
+  const [isSignupOpen, setIsSignupOpen] = React.useState(false);
 
-  console.log(data);
-
-  const [isopen, setIsopen] = useState(false);
-  const navigate = useNavigate();
-  const gotoHomepage = () => {
-    navigate("/");
-  };
-  function showModal() {
-    setIsopen(true);
-  }
-  function closeModal() {
-    setIsopen(false);
-  }
-
-  const getSignupVal = (event) => {
-    setData({ ...data, [event.target.name]: event.target.value });
+  const showSignupModal = () => {
+    setIsSignupOpen(true);
   };
 
-  const onSignup = async () => {
-    let eData = data.email.split("@");
-    // console.log(eData);
-    const username = eData[0];
-    console.log(username);
-    await axios
-      .post("http://localhost:1337/api/auth/local/register", {
-        ...data,
-        username: username,
-      })
-      .then((response) => {
-        console.log(response);
-        setData({});
-        closeModal();
-      })
-      .catch((err) => console.log(err));
+  const hideSignupModal = (data) => {
+    setIsSignupOpen(data);
   };
 
   return (
@@ -49,7 +21,6 @@ export default function Login() {
         <div className="login_div">
           <Form>
             <h1 style={{ textAlign: "center" }}>Login</h1>
-
             <Form.Field>
               <label>Email</label>
               <input placeholder="abc@gmail.com" />
@@ -68,71 +39,17 @@ export default function Login() {
             </p>
 
             <div className="log_sign_btn">
-              <button
-                className="login_btn"
-                onClick={() => {
-                  gotoHomepage();
-                }}>
-                Login
-              </button>
-              <button className="signup_btn" onClick={showModal}>
+              <button className="login_btn">Login</button>
+              <button className="signup_btn" onClick={showSignupModal}>
                 SignUp
               </button>
             </div>
           </Form>
         </div>
       </div>
-      <div className={isopen ? "showoverlay overlay" : "overlay"}></div>
-      <div className={isopen ? "show_signup_div signup_div" : "signup_div"}>
-        <div>
-          <Form onSubmit={onSignup}>
-            <h1 style={{ textAlign: "center" }}>SignUp</h1>
-            <Form.Field required={true}>
-              <label className="label_clr"> First Name</label>
-              <input
-                required={true}
-                placeholder="First Name"
-                type="text"
-                value={data.first_name || ""}
-                name="first_name"
-                onChange={getSignupVal}
-              />
-            </Form.Field>
-            <Form.Field required={true}>
-              <label>Last Name</label>
-              <input
-                required={true}
-                placeholder="Last Name"
-                type="text"
-                name="last_name"
-                onChange={getSignupVal}
-              />
-            </Form.Field>
-            <Form.Field required={true}>
-              <label>Email</label>
-              <input
-                type="email"
-                required={true}
-                placeholder="abc@gmail.com"
-                name="email"
-                onChange={getSignupVal}
-              />
-            </Form.Field>
-            <Form.Field required={true}>
-              <label>Password</label>
-              <input
-                required={true}
-                type="password"
-                placeholder="*******"
-                name="password"
-                onChange={getSignupVal}
-              />
-            </Form.Field>
-            <Button loading={false} className="signup_btn2" type="submit">
-              SignUp
-            </Button>
-          </Form>
-        </div>
+
+      <div className={""}>
+        {<Signup isopen={isSignupOpen} setIsOpen={hideSignupModal} />}
       </div>
     </>
   );
---
+++ src/auth/Signup.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { Button, Form } from "semantic-ui-react";
+import "./styles/Login.css";
+
+export default function Signup({ isopen = false, setIsOpen = () => {} }) {
+  const [data, setData] = useState({});
+
+  const getSignupVal = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+
+  const onSignup = async () => {
+    let eData = data.email.split("@");
+    const username = eData[0];
+    try {
+      // Commented out for now - API call with axios
+      // const response = await axios.post("http://localhost:1337/api/auth/local/register", {
+      //   ...data,
+      //   username: username,
+      // });
+      // console.log(response);
+
+      setData({});
+      // Add any additional logic here for successful signup, e.g., showing a success message.
+      // closeModal();
+      // Close the signup modal after successful signup
+    } catch (error) {
+      console.log(error);
+      // Handle signup error here, e.g., show an error message.
+    }
+  };
+
+  return (
+    <>
+      {" "}
+      <div className={isopen ? "showoverlay overlay" : "overlay"}></div>
+      <div className={isopen ? "show_signup_div signup_div " : "signup_div"}>
+        <div>
+          <Form onSubmit={onSignup}>
+            <h1 style={{ textAlign: "center" }}>SignUp</h1>
+            <Form.Field required={true}>
+              <label className="label_clr">First Name</label>
+              <input
+                required={true}
+                placeholder="First Name"
+                type="text"
+                value={data.first_name || ""}
+                name="first_name"
+                onChange={getSignupVal}
+              />
+            </Form.Field>
+            <Form.Field required={true}>
+              <label>Last Name</label>
+              <input
+                required={true}
+                placeholder="Last Name"
+                type="text"
+                name="last_name"
+                value={data.last_name || ""}
+                onChange={getSignupVal}
+              />
+            </Form.Field>
+            <Form.Field required={true}>
+              <label>Email</label>
+              <input
+                type="email"
+                required={true}
+                placeholder="abc@gmail.com"
+                name="email"
+                value={data.email || ""}
+                onChange={getSignupVal}
+              />
+            </Form.Field>
+            <Form.Field required={true}>
+              <label>Password</label>
+              <input
+                required={true}
+                type="password"
+                placeholder="*******"
+                name="password"
+                value={data.password || ""}
+                onChange={getSignupVal}
+              />
+            </Form.Field>
+            <Button loading={false} className="signup_btn2" type="submit">
+              SignUp
+            </Button>
+            <a
+              onClick={() => {
+                setIsOpen(false);
+              }}
+              style={{
+                float: "right",
+                marginTop: "0.5em",
+                cursor: "pointer",
+              }}>
+              Are you already a user?
+            </a>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+}

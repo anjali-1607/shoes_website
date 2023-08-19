25905c013a9fab9be4d38d0faaae182ea4419256
anjali-1607 <anjalirajpoot1609@gmail.com>
2023-08-20 00:08:20 +0530

    product_id cmplt

--- src/arrivals/ArrivalsBtn.js
+++ src/arrivals/ArrivalsBtn.js
@@ -4,18 +4,18 @@ import { ToastContainer, toast } from "react-toastify";
 import { useParams } from "react-router-dom";
 
 export default function ArrivalsBtn({ id }) {
-  const { id: productId } = useParams();
+  // const { id: productId } = useParams();
   const showToastMessage = () => {
     toast.success("Successfully Added to Cart ", {
       position: toast.POSITION.BOTTOM_RIGHT,
     });
   };
 
-  const postValue = async () => {
+  const postValue = async (productId) => {
     // console.log("anjali");
     const userId = localStorage.getItem("user_id");
     await secureAxios
-      .post(`/items/products_directus_users`, {
+      .post(`/items/products_directus_users   `, {
         directus_users_id: userId,
         products_id: productId,
       })
@@ -28,7 +28,11 @@ export default function ArrivalsBtn({ id }) {
     <>
       <ToastContainer />
       <div className="button_arr">
-        <button className="cart_arr_btn" onClick={postValue}>
+        <button
+          className="cart_arr_btn"
+          onClick={() => {
+            postValue(id);
+          }}>
           Add to Cart
         </button>
         <button className="buy_arr_btn">Buy Now</button>
